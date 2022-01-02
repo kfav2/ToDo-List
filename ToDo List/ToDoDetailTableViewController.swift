d89033308f2e272b1dc9d47ac48cfbf662abf21f
KFAV <kofava14@gmail.com>
2022-01-02 10:52:01 -0800

    Ch. 5 3 Working with Multiple View Controllers + Segues

---
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDo List
+//
+//  Created by Korlin Favara on 1/1/22.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
+   
+     override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        // code is asking the presentingViewController if it is a UINavigationController.
+        // UINavigationControllers present modally - have to dismiss
+        // otherwise view controller is not going through navigation controller - have to pop
+        // it should be noted these are all in a higher level navigatio controller
+        // isPresentingInAddMode becomes true or false
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            // although at this point you would not be in a UINavigationController, popViewController pops the top view controller from the stack
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
